dc42ac14c76315356da994d155c74e29f938b9ba
Dino Mandic <dinomandic@hotmail.com>
2017-12-26 13:40:44 -0600

    refactor: remove unneeded code from List component

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,15 +1,7 @@
 import React, { Component } from 'react';
-import openSocket from 'socket.io-client';
-import { Sparklines, SparklinesLine, SparklinesSpots  } from 'react-sparklines';
-import Trend from 'react-trend';
-import { Link } from 'react-router-dom';
-
-import { flash } from 'react-animations'
-
-
 import axios from 'axios';
 import numeral from 'numeral';
-import { Route, Redirect } from 'react-router';
+import { Link } from 'react-router-dom';
 
 
 import './List.css';
@@ -30,12 +22,6 @@ class List extends Component {
     }
   }
 
-  // componentWillMount() {
-  //   this.setState({
-  //     topGainers: 'FUUUJ'
-  //   })
-  // }
-
   componentDidMount() {
     axios.get("/api/user").then((res) => {
       this.setState({
@@ -43,69 +29,61 @@ class List extends Component {
       })
 
       axios.get(`/api/user/${this.state.user}/stocks`)
-      .then(res => {
-        this.setState({
-          watchlist: res.data
+        .then(res => {
+          this.setState({
+            watchlist: res.data
+          })
         })
-      })
     })
 
 
-      axios.get(`/api/top/${this.props.source}`)
+    axios.get(`/api/stocks/top/${this.props.source}`)
       .then(res => {
         this.setState({
           data: res.data
         })
-      }) 
-
-
+      })
 
   }
 
-
   render() {
-    console.log('My props', this.props.data);
-
-
+    
     return (
 
-            <div className={this.props.class}>
-              <div className="card-header"><span className={this.props.icon}></span>{this.props.title}</div>
-              <div className="card-body">
-                
-                <table className="table table-sm">
+      <div className={this.props.class}>
+        <div className="card-header"><span className={this.props.icon}></span>{this.props.title}</div>
+        <div className="card-body">
 
-                  <tbody>
-                    
-                    {this.state.data.map(item => {
-                      let symbol = '/company/' + item.symbol;
+          <table className="table table-sm">
 
-                      return <tr key={item.id}>
-                        
-                          <td>
-                          <Link className="my-class" to={symbol}>
-                            <div className="font-weight-bold">{item.symbol}</div>
-                          </Link>
-                            <div className="my-text">{item.companyName}</div>
-                          </td>
-                        
-                        
-                        {/* <td className="text-left">{item.companyName}</td> */}
-                        {
-                          item.changePercent > 0
-                            ?
-                            <td className="text-right"><h5 className="positive">{numeral(item.changePercent).format('0.00')}%</h5></td>
-                            :
-                            <td className="text-right"><h5 className="negative">{numeral(item.changePercent).format('0.00')}%</h5></td>
-                        }
+            <tbody>
 
-                        
-                      </tr>
-                    })}
-                  </tbody>
-                </table>
-              </div>
-            </div>     
+              {this.state.data.map(item => {
+                let symbol = '/company/' + item.symbol;
+
+                return <tr key={item.id}>
+
+                  <td>
+                    <Link className="my-class" to={symbol}>
+                      <div className="font-weight-bold">{item.symbol}</div>
+                    </Link>
+                    <div className="my-text">{item.companyName}</div>
+                  </td>
+
+                  {
+                    item.changePercent > 0
+                      ?
+                      <td className="text-right"><h5 className="positive">{numeral(item.changePercent).format('0.00')}%</h5></td>
+                      :
+                      <td className="text-right"><h5 className="negative">{numeral(item.changePercent).format('0.00')}%</h5></td>
+                  }
+
+                </tr>
+              })}
+            </tbody>
+          </table>
+        </div>
+      </div>
     )
   }
 
