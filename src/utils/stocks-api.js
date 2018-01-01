737e87ba49ba84ab0edbed4945094cc7707fb758
Dino Mandic <dinomandic@hotmail.com>
2017-12-31 19:51:31 -0600

    refactor: change endpoint

--- src/utils/stocks-api.js
+++ src/utils/stocks-api.js
@@ -5,7 +5,7 @@ const BASE_URL = 'http://localhost:3001';
 
 
 export function getStocksFromDb() {
-  const url = `${BASE_URL}/api/stocks`;
+  const url = `${BASE_URL}/api/stocks/`;
   return axios.get(url).then(res => res.data);
 }
 
